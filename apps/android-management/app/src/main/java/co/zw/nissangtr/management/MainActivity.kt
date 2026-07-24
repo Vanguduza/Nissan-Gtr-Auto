2bcd5a04547c3e574f9f5afb5e256b3002d8c890
Vanguduza <Vanguduza@users.noreply.github.com>
2026-07-24 23:24:48 +0200

    auto-sync: 2026-07-24 23:24:47

--- apps/android-management/app/src/main/java/co/zw/nissangtr/management/MainActivity.kt
+++ apps/android-management/app/src/main/java/co/zw/nissangtr/management/MainActivity.kt
@@ -102,7 +102,7 @@ class MainActivity : ComponentActivity() {
     @Deprecated("Deprecated in Java")
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray,
     ) {
         @Suppress("DEPRECATION")
