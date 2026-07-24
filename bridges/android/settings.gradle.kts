b0b8d0ff816f168621c8d67fc06454b1f77eec1a
Vanguduza <Vanguduza@users.noreply.github.com>
2026-07-24 22:49:51 +0200

    auto-sync: 2026-07-24 22:49:50

--- bridges/android/settings.gradle.kts
+++ bridges/android/settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "gtr-android-bridges"
 include(":location-tracker")
 project(":location-tracker").projectDir = file("location-tracker")
