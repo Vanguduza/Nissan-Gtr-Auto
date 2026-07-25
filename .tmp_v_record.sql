bdc50784f1ff69bb17598d7912342d3d76b09953
Vanguduza <Vanguduza@users.noreply.github.com>
2026-07-25 07:48:40 +0200

    auto-sync: 2026-07-25 07:48:40

--- .tmp_v_record.sql
+++
@@ -1,4 +0,0 @@
--- verifier apply: record migration versions after SQL files succeed
-INSERT INTO supabase_migrations.schema_migrations (version)
-VALUES ('20260724140000'), ('20260724150000')
-ON CONFLICT DO NOTHING;
