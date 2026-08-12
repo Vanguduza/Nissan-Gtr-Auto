4bb787e542f0981f64e9b58eb67d663eccd36433
Vanguduza <Vanguduza@users.noreply.github.com>
2026-08-12 05:15:15 +0200

    auto-sync: 2026-08-12 05:15:15

--- apps/android-management/feature/pos/src/main/java/co/zw/nissangtr/management/pos/PosScreen.kt
+++ apps/android-management/feature/pos/src/main/java/co/zw/nissangtr/management/pos/PosScreen.kt
@@ -6,11 +6,11 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.FlowRowScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
